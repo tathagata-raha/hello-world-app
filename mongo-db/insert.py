c3016bc330607aebd6cee0460e0bb623d091328d
Tathagata <tathagata.raha@research.iiit.ac.in>
2022-01-28 22:32:30 +0530

    Added comments

--- mongo-db/insert.py
+++ mongo-db/insert.py
@@ -1,8 +1,13 @@
+# importing librarires
 from pymongo import MongoClient
 import json
+# Initiating client
 client = MongoClient('mongodb+srv://tathagataraha:dfs-hello-world@cluster0.7ffec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+# opening db
 db = client.new_database
 collection = db.new_collection
+# Loading data from json file
 with open('data.json', 'r') as f:
     data = json.load(f)
+# Inserting data into MongoDB database
 collection.insert_many(data)
--- mongo-db/query.py
+++ mongo-db/query.py
@@ -1,12 +1,21 @@
+# importing librarires
 from pymongo import MongoClient
 import pprint
+# Initiating client
 client = MongoClient('mongodb+srv://tathagataraha:dfs-hello-world@cluster0.7ffec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+# opening db
+
 db = client.new_database
 collection = db.new_collection
+# sending queries to find a user with first name Giavani
 res = collection.find_one({'first_name': 'Giavani'})
 print("A person named Giavani:")
+# printing the result of the query
 pprint.pprint(res)
+# sending queries to finding users with gender female
 res = collection.find({'gender': 'Female'})
+# printing the result of the query
+
 print("All girls in database:")
 for girl in res:
     pprint.pprint(girl)
